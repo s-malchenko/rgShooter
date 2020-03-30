6779b56db765689b25451092b128e053bb86b1f0
s-malchenko <malchenko.sg@gmail.com>
2020-03-30 10:07:02 +0300

    Initial commit

---
+++ Source/rgShooter/rgShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "rgShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RGSHOOTER_API ArgShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
